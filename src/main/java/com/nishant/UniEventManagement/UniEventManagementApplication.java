c8edfce99dba77665e904fc08d3149e13f7c4446
NishantBudia <nishantbudia@gmail.com>
2023-09-26 11:55:09 +0530

    Initial commit

---
+++ src/main/java/com/nishant/UniEventManagement/UniEventManagementApplication.java
@@ -0,0 +1,13 @@
+package com.nishant.UniEventManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniEventManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniEventManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nishant/UniEventManagement/UniEventManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nishant.UniEventManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniEventManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
